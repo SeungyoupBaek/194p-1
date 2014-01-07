c58808064892df3f36f39b3360338fd5f5fa825f
SeungyoupBaek <bsy8220@naver.com>
2014-01-07 21:20:59 +0900

    Initial Commit

---
+++ 194p-1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  194p-1
+//
+//  Created by Youp on 2014. 1. 7..
+//  Copyright (c) 2014년 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
